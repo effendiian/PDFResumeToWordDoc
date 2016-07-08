51f31ef9b2ea1523f3a7952f7f82dd70b9cb9c64
Ian Effendi <effendiian+github@gmail.com>
2016-07-08 11:52:38 -0400

    Overview: Initial commit.

---
+++ StorageToWordDoc/StorageToWordDoc/Program.cs
@@ -0,0 +1,24 @@
+﻿// TODO: Licensing boilerplate.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageToWordDoc
+{
+	// TODO: Program.cs description.
+	public class Program
+	{
+
+		// TODO: Header.
+		public static void Main(string[] args)
+		{
+
+
+
+
+		}
+	}
+}
