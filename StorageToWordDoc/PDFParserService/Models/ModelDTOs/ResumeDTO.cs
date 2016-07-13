712ab307709088f41bde4f5b94f82ea7efa23020
Ian Effendi <effendiian+github@gmail.com>
2016-07-13 08:59:33 -0400

    Overview: Resume DTO creation of pseudocode.

---
+++ StorageToWordDoc/PDFParserService/Models/ModelDTOs/ResumeDTO.cs
@@ -0,0 +1,53 @@
+﻿// TODO: Boilerplate content.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PDFParserService.Models.ModelDTOs
+{
+	// TODO: Class description.
+	public class ResumeDTO
+	{
+		// Resume consisting of the information being parsed.
+
+		/* TODO: Ensure the following are true for ResumeDTO:
+		 * - "Resume" object.
+		 * - File property.
+		 * 
+		 * # Contact Information #
+		 * - Candidate's Name (Name object).
+		 * - Address (Address object).
+		 * - Email (Email Addess object).
+		 * - Home Telephone
+		 * - Work Telephone
+		 * - Mobile Telephone
+		 * 
+		 * # Experience #
+		 * - Company Name
+		 * - Position Title
+		 * - Description/Summary
+		 * - Dates
+		 * 
+		 * # Education #
+		 * - University
+		 * - Degree
+		 * - Major
+		 * - Minor 
+		 * - Dates
+		 * 
+		 * # Certifications #
+		 * - (bool) Is CFA?
+		 * 
+		 * # Licenses #
+		 * 
+		 * 
+		 * 	 
+		 */
+
+
+
+
+	}
+}
